40fc69fbd694866e0e3f23e684bec393690d5d5c
Henry Monge <henrymonge@gmail.com>
2024-11-06 08:04:02 -0500

    updated build_all.sh

--- frontier/hip_rocm_6_0_0/build_all.sh
+++ frontier/hip_rocm_6_0_0/build_all.sh
@@ -4,10 +4,10 @@ source ./env.sh
 # Seemingly QDPXX finds an installed one so no need to build this
 # ./build_libxml2.sh 
  
-./build_qmp.sh
-./build_hdf5.sh
-./build_qdpxx.sh
-./build_qdpxx_h5.sh
-./build_quda.sh
-./build_chroma_h5.sh
-./build_lalibe_gpu.sh
+./build_qmp.sh > out_build_qmp.sh 2>&1
+./build_hdf5.sh > out_build_hdf5.sh 2>&1
+./build_qdpxx.sh > out_build_qdpxx.sh 2>&1
+./build_qdpxx_h5.sh > out_build_qdpxx_h5.sh 2>&1
+./build_quda.sh > out_build_quda.sh 2>&1
+./build_chroma_h5.sh > out_build_chroma_h5.sh 2>&1
+./build_lalibe_gpu.sh > out_build_lalibe_gpu.sh 2>&1
