f87d74543674ca3fd555972c3c5beec6da3aef82
Henry Monge-Camacho <henrymonge@gmail.com>
2025-03-14 05:28:16 -0700

    update build_all

--- perlmutter/cuda/build_all.sh
+++ perlmutter/cuda/build_all.sh
@@ -2,6 +2,10 @@ source ./env.sh
 #
 # Seemingly QDPXX finds an installed one so no need to build this
 # ./build_libxml2.sh 
+
+pushd ../src
+./populate.sh
+popd
  
 ./build_qmp.sh
 ./build_qdpxx.sh
