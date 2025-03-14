576daad77c9bb0e5bb53c6e0d3c59e2116230341
Henry Monge-Camacho <henrymonge@gmail.com>
2025-03-14 05:35:26 -0700

    updated build_all

--- perlmutter/cuda/build_all.sh
+++ perlmutter/cuda/build_all.sh
@@ -3,6 +3,12 @@ source ./env.sh
 # Seemingly QDPXX finds an installed one so no need to build this
 # ./build_libxml2.sh 
 
+<< 'COMMENT'
+You may need to run this on a compute node.
+You can get an interactive allocation on perlmutter with
+salloc --nodes 1 --qos interactive --time 02:00:00 --constraint gpu --gpus 0 --account mxxxx
+COMMENT
+
 pushd ../src
 ./populate.sh
 popd
